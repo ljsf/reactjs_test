9d7533a86c0fabbf652bb39b488039025ec537b4
ljsf <2742070027@qq.com>
2019-09-12 16:38:00 +0800

    first commit

---
+++ precache-manifest.b3b372430eecd7025300e992219df0b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a5257070ecda4b8b9fb8f4091a6513",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf40b238fca97798d90",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "53b7c83cf3002ae5c5fc",
+    "url": "/static/js/2.99dfc5b5.chunk.js"
+  },
+  {
+    "revision": "5bf40b238fca97798d90",
+    "url": "/static/js/main.8c24725e.chunk.js"
+  },
+  {
+    "revision": "42d946f6c1b8b9257f1b",
+    "url": "/static/js/runtime~main.22e0fad0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
